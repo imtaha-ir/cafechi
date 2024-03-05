126dc459aa2f656ee9d0d2731399c280bba359c5
ImTaha <imtaha.ir@gmail.com>
2024-03-05 18:07:07 +0330

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "./images/logo.png";
+import hambugerImage from "./images/foods/Smoked-Hamburger.png";
+import beansImage from "./images/foods/loobia.jpeg";
+import "./App.css";
+import Header from "./components/header/header";
+
+function App() {
+  return (
+    <div>
+      <Header cafeName="Café You" instagram="cafeyou" logo={logo} />
+      <main className="menu-container">
+        <article className="menu-item">
+          <img src={hambugerImage} alt="" />
+          <div className="food-info">
+            <h3>همبرگر</h3>
+            <p>150 گرم گوشت چرخ کرده + سبزیجات</p>
+          </div>
+          <span>20</span>
+          <div className="food-price">120,000</div>
+        </article>
+        <article className="menu-item unavailable">
+          <img src={beansImage} alt="" />
+          <div className="food-info">
+            <h3>خوراک لوبیا</h3>
+            <p>لوبیا چیتی - قارچ - روغن زیتون</p>
+          </div>
+          <span>1</span>
+          <div className="food-price">60,000</div>
+        </article>
+      </main>
+
+      <footer className="">Footer</footer>
+    </div>
+  );
+}
+
+export default App;
